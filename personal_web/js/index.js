5cba5e916a660cbd7c1be055689e09c40c8311bf
s358411594 <s358411594@163.com>
2017-05-04 09:54:55 +0800

    add resume

--- personal_web/js/index.js
+++ personal_web/js/index.js
@@ -6,13 +6,14 @@
         //加载背景
         $(".bg").load('bg_particle.html',function(){
                 //点击切换页面2
+                    //进入游戏
                 $(".play_game").click(function(){
                     //加载首页之后的页面
-                    $(".pt-page-2").load('warcraft.html',function(){
+                    // $(".pt-page-2").load('warcraft.html',function(){
                         //随机切换
                         page_switch("pt-page-1","pt-page-2");
 
-                    });
+                    // });
                 });
         });
     }
